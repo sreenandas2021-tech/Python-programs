c1341e86d43ff21339aa594e01827ec786cb975a
sreenandas2021-tech <sreenandas2021@gmail.com>
2026-05-04 12:27:51 +0530

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+if operation == "+":
+    print("Result:", add(num1, num2))
+
+elif operation == "-":
+    print("Result:", subtract(num1, num2))
+
+elif operation == "*":
+    print("Result:", multiply(num1, num2))
+
+elif operation == "/":
+    print("Result:", divide(num1, num2))
+
+else:
+    print("Invalid choice")
